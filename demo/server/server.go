ebb112d926f33df1854fc816c715efd1edc64f09
Jorge Villatoro <jorge@justin.tv>
2014-04-10 10:50:54 -0700

    adding starvation tracker

--- demo/server/server.go
+++ demo/server/server.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"container/list"
 	"flag"
 	"fmt"
 	"github.com/thelazyfox/gortmp"
 	"github.com/thelazyfox/gortmp/log"
 	"github.com/zhangpeihao/goflv"
 	"net"
+	"time"
 )
 
 var (
@@ -48,6 +50,58 @@ func RecordStream(ms rtmp.MediaStream, filename string) {
 			if err != nil {
 				log.Error("RecordStream failed to write tag: %s", err)
 				return
+			}
+		}
+	}
+}
+
+func TrackStarvation(streamName string, ms rtmp.MediaStream) {
+	ch, err := ms.Subscribe()
+	if err != nil {
+		log.Error("Starvation tracker failed to subscribe to stream: %s", streamName)
+		return
+	}
+
+	defer ms.Unsubscribe(ch)
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	var curTs, lastTs int64
+	var lastClock time.Time
+	history := list.New()
+
+	for {
+		select {
+		case tag, ok := <-ch:
+			if ok {
+				if tag.Type == rtmp.VIDEO_TYPE {
+					curTs = int64(tag.Timestamp)
+				}
+			} else {
+				return
+			}
+		case <-ticker.C:
+			if lastClock.IsZero() {
+				lastTs = curTs
+				lastClock = time.Now()
+			} else {
+				streamDelta := (curTs - lastTs)
+				clockDelta := time.Since(lastClock).Nanoseconds() / 1000000
+				lastTs = curTs
+				lastClock = time.Now()
+				history.PushFront(clockDelta - streamDelta)
+
+				if history.Len() > 25 {
+					history.Remove(history.Back())
+				}
+
+				var sum int64
+
+				for e := history.Front(); e != nil; e = e.Next() {
+					sum += e.Value.(int64)
+				}
+
+				log.Info("TrackStarvation for stream %s - %d", streamName, sum/int64(history.Len()))
 			}
 		}
 	}
@@ -99,6 +153,7 @@ func (h *handler) OnPlay(rtmp.Stream) {
 
 func (h *handler) OnPublish(stream rtmp.Stream) {
 	// go RecordStream(h.server.GetMediaStream(stream.Name()), "out.flv")
+	go TrackStarvation(stream.Name(), h.server.GetMediaStream(stream.Name()))
 	fmt.Printf("OnPublish\n")
 }
 
